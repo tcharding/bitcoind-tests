66d63200956fd731ff2be2a5874ad8a05e1dff7c
Tobin C. Harding <me@tobin.cc>
2024-04-16 11:55:27 +1000

    Do trivia refactor to setup module

--- tests/setup/mod.rs
+++ tests/setup/mod.rs
@@ -2,13 +2,12 @@ extern crate miniscript;
 
 use bitcoind::bitcoincore_rpc::RpcApi;
 use bitcoind::BitcoinD;
-use miniscript::bitcoin;
 
 pub mod test_util;
 
 // Launch an instance of bitcoind with
 pub fn setup() -> BitcoinD {
-    // Create env var BITCOIND_EXE_PATH to point to the ../bitcoind/bin/bitcoind binary
+    // Create env var BITCOIND_EXE_PATH to point to the ../bin/bitcoind binary
     let key = "BITCOIND_EXE";
     if std::env::var(key).is_err() {
         let mut root_path = std::env::current_dir().unwrap();
@@ -25,14 +24,19 @@ pub fn setup() -> BitcoinD {
     let exe_path = bitcoind::exe_path().unwrap();
     let bitcoind = bitcoind::BitcoinD::new(exe_path).unwrap();
     let cl = &bitcoind.client;
-    // generate to an address by the wallet. And wait for funds to mature
+
+    // Generate to an address for the wallet and wait for funds to mature.
+    //
+    // This also sanity checks that bitcoind is up and we can chat to it over RPC.
     let addr = cl.get_new_address(None, None).unwrap().assume_checked();
     let blks = cl.generate_to_address(101, &addr).unwrap();
     assert_eq!(blks.len(), 101);
 
+    // Sanity check that we have coins ready to go.
     assert_eq!(
         cl.get_balance(Some(1) /*min conf*/, None).unwrap(),
         bitcoin::Amount::from_sat(100_000_000 * 50)
     );
+
     bitcoind
 }
